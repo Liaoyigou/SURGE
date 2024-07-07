612e93ce4293fc7754edec7e80b3397a70d9c300
PAN <86490269+Liaoyigou@users.noreply.github.com>
2024-07-07 21:10:49 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-// 修改响应内容以解锁会员功能
+// script.js 文件内容，修改响应数据以解锁会员功能
+
 let body = JSON.parse($response.body);
 
-// 伪造一个成功的内购验证响应
+// 打印原始响应内容，供调试使用
+console.log("Original response body:", JSON.stringify(body, null, 2));
+
+// 修改响应内容以解锁会员功能
 body = {
   "status": "success",
   "membership": {
@@ -10,4 +14,8 @@ body = {
   }
 };
 
+// 打印修改后的响应内容，供调试使用
+console.log("Modified response body:", JSON.stringify(body, null, 2));
+
+// 返回修改后的响应
 $done({body: JSON.stringify(body)});
